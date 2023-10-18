94de745b3e7232e0ea5e77574aa8ccf56063a539
d-laslo <ice_bear@ukr.net>
2023-10-18 17:43:59 +0300

    updated parameters

--- chip8_emu_rust/src/chip8/parameters.rs
+++ chip8_emu_rust/src/chip8/parameters.rs
@@ -1,5 +1,6 @@
-pub const SCREEN_WIDTH : usize = 128;
-pub const SCREEN_HEIGH : usize = 64;
-pub const MEMORY_SIZE : usize = 0xFFF;
-pub const STACK_SIZE : usize = 16;
-pub const REGISTERS_COUNT : usize = 16;
+pub const SCREEN_WIDTH : usize = 0x80;
+pub const SCREEN_HEIGH : usize = 0x40;
+pub const MEMORY_SIZE : usize = 0x1000;
+pub const STACK_SIZE : usize = 0x10;
+pub const REGISTERS_COUNT : usize = 0x10;
+pub const KEYS_COUNT : usize = 0x10;
